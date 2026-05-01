8825534660958c90d291b69d6b5380cb8bfdfb52
sandhuliza18 <liza3@illinois.edu>
2026-04-30 22:46:31 -0500

    Delete restriction.m

--- restriction.m
+++
@@ -1,9 +0,0 @@
-function R = restriction(nb,boundary_list);
-
-
-all_nodes = [1:nb]';
-interior  = setdiff(all_nodes,boundary_list);
-
-R         = speye(nb);
-R         = R(interior,:); %% Keep only interior columns
-
